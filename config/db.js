abbef1b5c196794b43d45b4940af1980b8d4ffc8
kajalm22 <mishrakajalr22@gmail.com>
2022-11-28 15:48:39 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Database connected ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
-    process.exit(1);
+    console.log(error)
+    process.exit(1)
   }
-};
+}
 
 module.exports = connectDB;
